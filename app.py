105a2dc8c11a9cbe5c27da16594c60f628f72b0b
Sai Mounika <battinasaimounika558@gmail.com>
2025-06-01 17:39:54 +0530

    Fix: remove local path from requirements.txt for Render deployment

--- app.py
+++ app.py
@@ -86,4 +86,6 @@ def upload_audio():
 
 # ----------------- MAIN -----------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
